a915b9d07ce8792e934d30cd6c0d2d5738b82b87
blr3 <32915007+blr3@users.noreply.github.com>
2021-05-19 00:44:34 -0500

    Add files via upload

--- websiteFiles/JS/lightbox.js
+++ websiteFiles/JS/lightbox.js
@@ -1,3 +1,6 @@
-const lightbox = document.createElement('div');
-lightbox.id = 'myModal';
-document.body.appendChild(lightbox);
+const lightbox1 = document.createElement('div');
+const lightbox2 = document.createElement('div');
+lightbox1.id = 'myModal';
+lightbox2.id = 'fsModal';
+document.body.appendChild(lightbox1);
+document.body.appendChild(lightbox2);
